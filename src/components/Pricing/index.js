aa3d656fa2b30a9ea6504b04de9aaf5d11f5a4f6
Nerati Akhil Reddy <akhil990836@gmail.com>
2024-05-28 16:01:48 +0530

    pricing

--- src/components/Pricing/index.js
+++ src/components/Pricing/index.js
@@ -1,21 +1,16 @@
 import {Component} from 'react'
-import React from "react";
-import Switch from "react-switch";
-import { FaCheck } from "react-icons/fa";
+import React from "react"
+import Switch from "react-switch"
+import { FaCheck } from "react-icons/fa"
 import './index.css'
 
 class Pricing extends Component{
-    constructor() {
-        super();
-        this.state = { checked: false };
-        this.handleChange = this.handleChange.bind(this);
-      }
-    
-      handleChange(checked) {
-        this.setState({ checked });
-      }
+    state = {rangeValue: 50}
+
+    onChangeSlider = event => this.setState({rangeValue: event.target.value})
 
     render(){
+        const {rangeValue} = this.state
         return (
             <div className='pricing-container'>
                 <h1 className='heading'>Simple, traffic-based pricing</h1>
@@ -23,11 +18,13 @@ class Pricing extends Component{
                 <div className='pricing-content'>
                     <div className='amount-section'>
                         <p className='page-views'>100K PAGEVIEWS</p>
+                        <input type='range' min={0} max={100} value={rangeValue} onChange={this.onChangeSlider} className='range-slider mobile' />
                         <div className='rate'>
-                          <h1 className='amount'>$16.00</h1>
+                          <h1 className='amount'>${rangeValue}</h1>
                           <p className='span-txt'>/month</p>
                         </div>
                     </div>
+                    <input type='range' min={0} max={100} value={rangeValue} onChange={this.onChangeSlider} className='range-slider desktop' />
                     <div className='billing-section'>
                         <p className='billing-txt'>Monthly Billing</p>
                         <label className='switch'>
